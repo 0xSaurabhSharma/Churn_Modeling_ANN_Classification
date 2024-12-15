d5e6398aa0a07aeeb8719d7f716c0f408b45ff71
Saurabh Sharma <101334796+0xSaurabhSharma@users.noreply.github.com>
2024-12-15 08:59:17 +0000

    ADD: Create streamlit app for regression model

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Churn_Modeling_ANN_Classification
+
+Try it at [here on Streamlit Web-App](https://churn-modeling-ann-classification.streamlit.app/)
--- streamlit_regression.py
+++ streamlit_regression.py
@@ -5,12 +5,10 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
-# Load the trained model
-model = tf.keras.models.load_model('model.h5')
+# load the trained model
+model = tf.keras.models.load_model('regression_model.h5')
 
-print(model.summary())
-
-# Load the encoders and scaler
+# load the encoders and scalers
 with open('label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
@@ -20,21 +18,20 @@ with open('onehot_encoder_geo.pkl', 'rb') as file:
 with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
+# Streamlit app
+st.title('Estimated Salary Prediction')
 
-## streamlit app
-st.title('Customer Churn Prediction')
-
-# User input
+# User Input
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
 balance = st.number_input('Balance')
 credit_score = st.number_input('Credit Score')
-estimated_salary = st.number_input('Estimated Salary')
 tenure = st.slider('Tenure', 0, 10)
-num_of_products = st.slider('Number of Products', 1, 4)
-has_cr_card = st.selectbox('Has Credit Card', [0, 1])
-is_active_member = st.selectbox('Is Active Member', [0, 1])
+num_of_products = st.slider('Number of PRoducts', 1, 4)
+has_cr_card = st.selectbox('Has Credit Card', [0,1])
+is_active_member = st.selectbox('Is Active Member ?', [0,1])
+exited = st.selectbox('Exited',[0,1])
 
 # Prepare the input data
 input_data = pd.DataFrame({
@@ -46,10 +43,10 @@ input_data = pd.DataFrame({
     'NumOfProducts': [num_of_products],
     'HasCrCard': [has_cr_card],
     'IsActiveMember': [is_active_member],
-    'EstimatedSalary': [estimated_salary]
+    'Exited': [exited]
 })
 
-# One-hot encode 'Geography'
+# One hot encode Geography
 geo_encoded = onehot_encoder_geo.transform([[geography]]).toarray()
 geo_encoded_df = pd.DataFrame(geo_encoded, columns=onehot_encoder_geo.get_feature_names_out(['Geography']))
 
@@ -59,17 +56,10 @@ input_data = pd.concat([input_data.reset_index(drop=True), geo_encoded_df], axis
 # Scale the input data
 input_data_scaled = scaler.transform(input_data)
 
+#Predict churn
+sal_prediction = model.predict(input_data_scaled)
+sal_prediction_proba = sal_prediction[0][0]
 
-# Predict churn
-prediction = model.predict(input_data_scaled)
-prediction_proba = prediction[0][0]
+st.write(f'Estimated Salary: {sal_prediction_proba:.2f}')
+print(f'Estimated Salary: {sal_prediction_proba:.2f}')
 
-st.write(f'Churn Probability: {prediction_proba:.2f}')
-print(f'Churn Probability: {prediction_proba:.2f}')
-
-if prediction_proba > 0.5:
-    st.write('The customer is likely to churn.')
-    print('Yess')
-else:
-    st.write('The customer is not likely to churn.')
-    print('Noo')
